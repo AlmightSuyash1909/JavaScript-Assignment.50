c0a5c0309c6f593322302f341789fb3cf61459a0
AlmightSuyash1909 <suyashprime1909@gmail.com>
2024-04-08 23:09:24 +0530

    Cleane Code

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1. Refactor the code below following the guidelines
+
+   // (1)  const car = {
+   //      carMake: 'Honda',
+   //      carModel: 'Accord',
+   //      carColor: 'Blue',
+   //      };
+   //      function paintCar(car) {
+   //      car.carColor = 'Red';
+   //             
+
+   //      const car = {
+   //      make:"Honda",  
+   //      model: "Accord",
+   //      color:"Blue",
+   //      }
+   //      function paintCar (car) {
+   //      return car.color="red"
+   //      }
+
+   // (2)  let pi = 3.14;
+   //      let area = 2 * pi * 21;
+   //      console.log(area);
+
+   //      const pi = 3.14;
+   //      let area = 2* pi * 21 ;
+   //      console.log(area)
+
+   // (3)   function addingTwoNumbers(numA, numB) {
+   //       return numA + numB;
+   //       }
+   //       addingTwoNumbers(100);
+   //       addingTwoNumbers(100, 200);
+
+   //       function addingTwoNumbers(numA=0,numB=0) {
+   //       return numA+ numB
+   //       }
+   //       console.log(addingTwoNumbers(1000))
+   //       console.log(addingTwoNumbers(1000,1000))
